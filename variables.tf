e026ed920a3e152aad3311930a1842753d494c0a
Harald Vinje <haraldvinje@gmail.com>
2023-08-07 08:24:06 +0200

    Removing named profile, as it is not needed.

--- variables.tf
+++ variables.tf
@@ -2,10 +2,6 @@ variable "region" {
   default = "eu-west-1"
 }
 
-variable "profile" {
-  default = "default"
-}
-
 variable "generated_key_name" {
   type        = string
   default     = "my-ec2-ubuntu-key-pair"
